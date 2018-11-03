e17a811d2200f15f731b4ce14320b8d38623efd0
Frank Carnovale <frank.carnovale@gmail.com>
2018-11-03 09:32:26 +0000

    cleaner cleaner

--- clean-rancher.sh
+++ clean-rancher.sh
@@ -19,7 +19,7 @@ fi
 echo pruning images..
 docker image prune -f
 
-cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico"
+cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico /var/log/contaiers /var/log/pods"
 for dir in $cleanupdirs; do
     if [ -d $dir ]; then
         echo removing $dir..
@@ -31,12 +31,6 @@ mounts=$(df | awk '/kubelet/ {print $6}')
 if [ -n "$mounts" ]; then
     echo unmounting all kubelets..
     umount $mounts
-fi
-
-logs=$(ls /var/log/containers 2>/dev/null)
-if [ -n "$logs" ]; then
-    echo cleaning out all /var/log/containers..
-    rm -rf $logs
 fi
 
 echo I have cleaned up the mess that rancher made.
